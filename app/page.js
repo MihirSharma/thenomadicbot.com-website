c29ca088aa14519c90d91f78747a9831ce4390af
Mihir Sharma <mihirsharmaofficial@gmail.com>
2023-06-30 02:55:32 +0530

    css 21

--- app/page.js
+++ app/page.js
@@ -2,11 +2,26 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Loading from "./loading";
 import HomePage from "./(HomeComponents)/HomePage";
+import Head from "next/head";
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<HomePage />
-		</main>
+		<>
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossorigin
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<main className={styles.main}>
+				<HomePage />
+			</main>
+		</>
 	);
 }
