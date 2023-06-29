8db7013abfce5b9fcb392aa7fd7f7d748b527e6f
Mihir Sharma <mihirsharmaofficial@gmail.com>
2023-06-30 01:26:34 +0530

    responsive 5

--- app/(HomeComponents)/HomePage.js
+++ app/(HomeComponents)/HomePage.js
@@ -25,36 +25,16 @@ const HomePage = () => {
 			</div> */}
 			<div class="container">
 				<div class="stack" id="stack-main">
-					<span
-						style={{
-							fontFamily: "Space Mono",
-							textAlign: "center",
-						}}
-						id="stack-1">
+					<span style={{ fontFamily: "Space Mono" }} id="stack-1">
 						THE NOMADIC BOT
 					</span>
-					<span
-						style={{
-							fontFamily: "Space Mono",
-							textAlign: "center",
-						}}
-						id="stack-2">
+					<span style={{ fontFamily: "Space Mono" }} id="stack-2">
 						THE NOMADIC BOT
 					</span>
-					<span
-						style={{
-							fontFamily: "Space Mono",
-							textAlign: "center",
-						}}
-						id="stack-3">
+					<span style={{ fontFamily: "Space Mono" }} id="stack-3">
 						THE NOMADIC BOT
 					</span>
-					<span
-						style={{
-							fontFamily: "Space Mono",
-							textAlign: "center",
-						}}
-						id="stack-4">
+					<span style={{ fontFamily: "Space Mono" }} id="stack-4">
 						THE NOMADIC BOT
 					</span>
 				</div>
