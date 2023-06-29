8828efc9f540d2791882e2fbbbab4e3a26179aa7
Mihir Sharma <mihirsharmaofficial@gmail.com>
2023-06-30 03:10:23 +0530

    favicon

--- app/page.js
+++ app/page.js
@@ -2,26 +2,11 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Loading from "./loading";
 import HomePage from "./(HomeComponents)/HomePage";
-import Head from "next/head";
 
 export default function Home() {
 	return (
-		<>
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossorigin
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-			<main className={styles.main}>
-				<HomePage />
-			</main>
-		</>
+		<main className={styles.main}>
+			<HomePage />
+		</main>
 	);
 }
